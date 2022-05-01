f05720d876d6f2131e6fd74f5410cd390ab60040
Joohyegyeong <hyegyeong25@gmail.com>
2022-05-01 18:28:33 +0900

    22.05.01 - 아스키코드 숫자변환

---
+++ src/main/java/Ex06_String/Ex11654.java
@@ -0,0 +1,12 @@
+package Ex06_String;
+
+import java.util.Scanner;
+
+public class Ex11654 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char alphabet = scan.next().charAt(0);
+
+        System.out.println((int)alphabet);
+    }
+}
