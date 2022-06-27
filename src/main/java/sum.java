34087a71c115aaba453dda1a036ea8873e718a2c
Joohyegyeong <hyegyeong25@gmail.com>
2022-06-27 22:28:48 +0900

    22.06.27 - 함수

---
+++ src/main/java/sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.println(plus(12,87));
+    }
+    public static int plus(int a, int b){
+        return a+b;
+    }
+}
